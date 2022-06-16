61b62cc29f59bc5a024ab7016de90c688b54e10d
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-16 14:19:21 -0500

    added event listener for click event on dog walkers

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,5 +1,26 @@
 import { getWalkers } from "./database.js"
 
+
+//this is a click event, it's eliciting an response from an action (the "click")
+document.addEventListener( 
+    //param 1= action name -> this is a defined name 
+    "click",
+    //param 2= type of event -> can name this whatever i want
+    (clickEvent) => {
+        const itemClicked = clickEvent.target //tageting the name in the param
+        if (itemClicked.id.startsWith("walker")) { //checking if the id from the for of loop below starts with the walker
+            const [,walkerId] = itemClicked.id.split("--") //splits the id="walker" from the ${walker.id}, using "," from the for of loop below. 
+
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) { //turns the string from line 16 into an integer
+                    window.alert(`${walker.name} services ${walker.city}`)
+                }
+            }
+        }
+    }
+)
+
+
 const walkers = getWalkers()
 
 
@@ -7,11 +28,11 @@ export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>` //HTML representation of of list items
     }
     
     walkerHTML += "</ul>"
-    
+
     return walkerHTML
 }
 
